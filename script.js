96ff5da810e3a944928c19b204bba6f3b01f058d
Ivan <kwakiovoono@gmail.com>
2022-08-15 21:51:25 +0200

    Added variable grid square size

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ const container = document.querySelector('.container')
 let mouseDown = false;
 container.addEventListener('mousedown', () => mouseDown = true);
 container.addEventListener('mouseup', () => mouseDown = false);
+let gridSize = 100;
+let squareWidth = 640 / gridSize - 2;
 
 
-for (let i = 0; i < 256; i++){
+for (let i = 0; i < gridSize*gridSize; i++){
     const square = document.createElement('div');
     square.className = 'square';
-    square.style.width = '38px'
-    square.style.height = '38px';
+    square.style.width = `${squareWidth}px`;
+    square.style.height = `${squareWidth}px`;
     container.appendChild(square);
     square.addEventListener('mouseover', changeColor);
-    square.addEventListener('mousedown', changeColor);
-    
+    square.addEventListener('mousedown', changeColor);    
 }
 
 function changeColor(e) {
