3c2e30d4c79ab31b8258f8369483397f27698a5c
Ivan <kwakiovoono@gmail.com>
2022-08-28 21:08:36 +0200

    Add clear button

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 const container = document.querySelector('.container')
 const slider = document.querySelector('.slider')
 const sliderOutputValue = document.querySelector('#sliderValue')
+const clearButton = document.querySelector('.clear')
 
 let mouseDown = false;
 container.addEventListener('mousedown', () => mouseDown = true);
 container.addEventListener('mouseup', () => mouseDown = false);
 slider.addEventListener('input', updateSliderOutput);
 slider.addEventListener('change', updateSize);
+clearButton.addEventListener('click', clearGrid);
 let numberOfSquares;
 
 drawBoard(16);
@@ -43,5 +45,12 @@ function updateSize(e) {
 function clearContainer() {
     for (let i = 0; i < numberOfSquares; i++) {
         document.querySelector('.square').remove();
+    }
+}
+
+function clearGrid() {
+    squares = document.querySelectorAll('.square');
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = '#36454f';
     }
 }
